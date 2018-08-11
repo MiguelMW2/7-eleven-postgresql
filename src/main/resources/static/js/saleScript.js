37bc31d8505aae448e76baccd13c7450ab5dc479
Miguel <miguelangelsonarc@hotmail.com>
2018-08-11 00:51:25 -0500

    commit

--- src/main/resources/static/js/saleScript.js
+++ src/main/resources/static/js/saleScript.js
@@ -1,3 +1,31 @@
-$(document).ready(function() {
-	console.log("ready!");
-});
+
+	function buscarProducto() {
+		console.log("ready!");
+		
+		$.ajax({
+			type : "GET",
+			contentType : "application/json",
+			url : window.location + "rest/product/search/",
+			data : JSON.stringify(formData),
+			dataType : 'json',
+			success : function(result) {
+				
+				console.log("success");
+				/*if(result.status == "Done"){
+					$("#postResultDiv").html("<p style='background-color:#7FA7B0; color:white; padding:20px 20px 20px 20px'>" + 
+												"Post Successfully! <br>" +
+												"---> Customer's Info: FirstName = " + 
+												result.data.firstname + " ,LastName = " + result.data.lastname + "</p>");
+				}else{
+					$("#postResultDiv").html("<strong>Error</strong>");
+				}
+				console.log(result);
+				*/
+			},
+			error : function(e) {
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+		});
+		
+	}
