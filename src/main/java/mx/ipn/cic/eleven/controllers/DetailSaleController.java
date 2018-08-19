4349cfbaf7e852bcda267077ac8a69f06077d8b8
Unknown <miguelangelsonarc@hotmail.com>
2018-08-19 14:04:10 -0500

    commit

--- src/main/java/mx/ipn/cic/eleven/controllers/DetailSaleController.java
+++ src/main/java/mx/ipn/cic/eleven/controllers/DetailSaleController.java
@@ -3,26 +3,28 @@ package mx.ipn.cic.eleven.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import mx.ipn.cic.eleven.entities.DetailSaleEntity;
 import mx.ipn.cic.eleven.services.DetailSaleService;
 
-@Controller
-@RequestMapping(path="/detailSale")
+@RestController
+@RequestMapping(path="/detailSale", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 public class DetailSaleController {
 
 	@Autowired
 	private DetailSaleService detailSaleService;
 
 	@PostMapping(path="/save")
-	public String register(@RequestBody List<Object> detailSaleList) {
+	public String register(@RequestBody List<DetailSaleEntity> detailSale) {
 		/*this.detailSaleService.saveAll(detailSaleList);
 		return "redirect:/sale/payment/";*/
-		System.out.println(detailSaleList);
-		return "redirect:/sale/payment/";
+		System.out.println(detailSale);
+		return "redirect:/sale/payment";
 	}
 }
